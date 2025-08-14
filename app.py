37061e438495439eb5f759cc6b9ecec6325e35cb
ZataraHere <shabazekhan768@gmail.com>
2025-08-15 00:05:10 +0530

    edited helper and main file

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocess.preprocess(df, region_df)
 
+st.sidebar.title("Olympics Analysis")
+st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
 user_select = st.sidebar.radio(
     'select an option',
     ('Medal Tally', 'Overall Analysis','Country Wise Analysis','Overall Analysis')
@@ -18,5 +20,12 @@ user_select = st.sidebar.radio(
 st.dataframe(df)
 
 if user_select == 'Medal Tally':
+
+    st.sidebar.header("Medal Tally")
+    years,country = helper.country_years_list(df)
+
+    selected_year = st.sidebar.selectbox("Select Year",years)
+    selected_country = st.sidebar.selectbox("Select Country", country)
+
     medal_tally = helper.medal_tally(df)
     st.dataframe(medal_tally)
--- helper.py
+++ helper.py
@@ -1,5 +1,18 @@
+import numpy as np
 def medal_tally(df):
     medal_tally = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'])
     medal_tally = medal_tally.groupby('NOC').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold', ascending=False).reset_index()
     medal_tally['Total'] = medal_tally['Gold'] + medal_tally['Silver'] + medal_tally['Bronze']
     return medal_tally
+
+def country_years_list(df):
+    df.dropna(subset=['Year'], inplace=True)
+    years = df['Year'].astype('int').unique().tolist()
+    years.sort()
+    years.insert(0, 'Overall')
+
+    country = np.unique(df['region'].dropna().values).tolist()
+    country.sort()
+    country.insert(0, 'Overall')
+
+    return  years, country
