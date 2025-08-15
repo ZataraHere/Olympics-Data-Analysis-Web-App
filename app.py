cfcbefc84475c52467aeb471ad66796c45a70b5b
ZataraHere <shabazekhan768@gmail.com>
2025-08-15 17:40:06 +0530

    added fetch tally function in helper

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocess.preprocess(df, region_df)
 
-st.sidebar.title("Olympics Analysis")
 st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
+st.sidebar.title("Olympics Analysis")
+st.sidebar.image('https://wallpapers.com/images/hd/men-s-running-event-olympic-sports-ofh5ckbutl16vpaz.jpg')
 user_select = st.sidebar.radio(
     'select an option',
     ('Medal Tally', 'Overall Analysis','Country Wise Analysis','Overall Analysis')
 )
 
 
-st.dataframe(df)
+#st.dataframe(df)
 
 if user_select == 'Medal Tally':
 
@@ -27,5 +28,14 @@ if user_select == 'Medal Tally':
     selected_year = st.sidebar.selectbox("Select Year",years)
     selected_country = st.sidebar.selectbox("Select Country", country)
 
-    medal_tally = helper.medal_tally(df)
-    st.dataframe(medal_tally)
+    medal_tally = helper.fetch_medal_tally(selected_year, selected_country, df)
+    if selected_year == 'Overall' and selected_country == 'Overall':
+        st.title('Overall Tally')
+    if selected_year != 'Overall' and selected_country == 'Overall':
+        st.title("Medal Tally in "+str(selected_year)+" Olympics")
+    if selected_year == 'Overall' and selected_country != 'Overall':
+        st.title(selected_country+"'s Overall Performance")
+    if selected_year != 'Overall' and selected_country != 'Overall':
+        st.title(selected_country+"'s Overall Performance in "+str(selected_year)+" Olympics")
+
+    st.table(medal_tally)
--- helper.py
+++ helper.py
@@ -16,3 +16,26 @@ def country_years_list(df):
     country.insert(0, 'Overall')
 
     return  years, country
+
+def fetch_medal_tally(year, country, df):
+    flag = 0
+
+    medal_df = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'])
+    medal_df['Year'] = medal_df['Year'].astype('int')
+    if year == 'Overall' and country == 'Overall':
+        temp_df = medal_df
+    if year != 'Overall' and country == 'Overall':
+        temp_df = medal_df[medal_df['Year'] == year]
+    if year == 'Overall' and country != 'Overall':
+        flag = 1
+        temp_df = medal_df[medal_df['region'] == country]
+    if year != 'Overall' and country != 'Overall':
+        temp_df = medal_df[(medal_df['Year'] == year) & (medal_df['region'] == country)]
+
+    if flag == 1:
+        x = temp_df.groupby('Year').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Year').reset_index()
+    else:
+        x = temp_df.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold', ascending=False).reset_index()
+
+    x['Total'] = x['Gold'] + x['Silver'] + x['Bronze']
+    return  x
